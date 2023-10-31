31047a86a9f2c6d31b7065c1a8c3b5a5931615bb
Matheus Schuch <102114294+MatheusShu1@users.noreply.github.com>
2023-10-30 21:47:54 -0300

    .
    .

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,7 +6,7 @@ from recipes.views import home, login
 urlpatterns = [
     path('login', login),
     path('home', home),
-    path('cliente/cadastro', cliente_cadastro),
-    path('funcionario/cadastro', funcionario_cadastro),
-    path('fornecedor/cadastro', fornecedor_cadastro),
+    path('cliente/cadastro', cliente_cadastro, name='cliente_cadastro'),
+    path('funcionario/cadastro', funcionario_cadastro, name='funcionario_cadastro'),
+    path('fornecedor/cadastro', fornecedor_cadastro, name='fornecedor_cadastro'),
 ]
