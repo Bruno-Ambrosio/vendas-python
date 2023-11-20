28e40f51563fae98ba802783568f28e06e8729e0
Matheus Schuch <102114294+MatheusShu1@users.noreply.github.com>
2023-11-20 20:23:57 -0300

    modalEdit
    adicionado o modal de edição dos items

--- recipes/urls.py
+++ recipes/urls.py
@@ -12,5 +12,8 @@ urlpatterns = [
     path('cliente/consulta', cliente_consulta, name='cliente_consulta'),
     path('fornecedor/consulta', fornecedor_consulta, name='fornecedor_consulta'),
     path('funcionario/consulta', funcionario_consulta, name='funcionario_consulta'),
+    path('cliente/cadastro/<int:cliente_id>/', cliente_cadastro, name='cliente_cadastro'),
+    path('fornecedor/cadastro/<int:fornecedor_id>/', fornecedor_cadastro, name='fornecedor_cadastro'),
+    path('funcionario/cadastro/<int:funcionario_id>/', funcionario_cadastro, name='funcionario_cadastro')
 
 ]
